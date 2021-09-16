2714531a31fe63556ab6ed6f1038b33debce17bb
brunocosta1 <brunocosta.cg1998@gmail.com>
2021-09-16 08:32:29 -0300

    Configuração inicial

---
+++ Main.hs
@@ -0,0 +1,10 @@
+data Stack = Stack { TAM_MAX :: Int, --tamanho máximo
+                     valores :: [Int], --vetor valores
+                     topo :: Int -- topo
+
+    }deriving(Show)
+
+
+Inicializa :: Stack -> Stack -> Stack
+Inicializa tam 
+
